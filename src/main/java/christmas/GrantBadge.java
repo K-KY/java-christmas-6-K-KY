4ae1ab1f605ba417b094e6c80645eec1139f2fe7
K-KY <kyuyoungk@naver.com>
2023-11-12 03:46:03 +0900

    Refactor(GrantBadge) : 불필요한 메서드 삭제

--- src/main/java/christmas/GrantBadge.java
+++ src/main/java/christmas/GrantBadge.java
@@ -1,16 +1,11 @@
 package christmas;
 
+import christmas.view.OutputView;
+
 public class GrantBadge {
     public ChristmasBadge grantedBadge(int point) {
         ChristmasBadge badge = ChristmasBadge.getBadge(point);
-        badgeAnnouncement(badge);
+        OutputView.badgeAnnouncement(badge);
         return badge;
-    }
-
-    private void badgeAnnouncement(ChristmasBadge badge) {
-        if (badge != ChristmasBadge.NO_BADGE) {
-            System.out.println("고객님은 " + badge.getName(badge) +" 배지를 획득 하셨습니다!");
-            System.out.println("이후에 진행 될 새해 이벤트에서 배지에 따른 선물이 증정됩니다.");
-        }
     }
 }
