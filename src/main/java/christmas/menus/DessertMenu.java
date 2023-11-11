937b1615204590c145cac4728b994e0e225e0b45
K-KY <kyuyoungk@naver.com>
2023-11-12 02:27:17 +0900

    Feat(DessertMenu) : MenuBoard에 들어갈 디저트 메뉴 맵을 리턴하는 enum 클래스
    menuBoard가 비어있을 때만 해시맵을 생성한다.

---
+++ src/main/java/christmas/menus/DessertMenu.java
@@ -0,0 +1,32 @@
+package christmas.menus;
+
+import christmas.MenuBoard;
+import java.util.HashMap;
+import java.util.List;
+
+public enum DessertMenu {
+    CHOCOLATE_CAKE("초코케이크", 15_000),
+    ICE_CREAM("아이스크림", 5_000);
+
+    private static final HashMap<String, Integer> menuBoard = new HashMap<>();
+    private static final String MENU_TYPE = "DESSERT";
+    private final String menuName;
+    private final int price;
+
+    DessertMenu(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    private static void setMenuBoard() {
+        List<DessertMenu> menus = List.of(DessertMenu.values());
+        menus.forEach(menu -> menuBoard.put(menu.menuName, menu.price));
+    }
+
+    public static MenuBoard getMenuBoard() {//Category로 변경
+        if (menuBoard.isEmpty()) {
+            setMenuBoard();
+        }
+        return new MenuBoard(menuBoard, MENU_TYPE);
+    }
+}
