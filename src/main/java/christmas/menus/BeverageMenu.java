c98947c6a1cc36b0fb275ba876e6bc2df928ee4a
K-KY <kyuyoungk@naver.com>
2023-11-12 02:08:15 +0900

    Feat(BeverageMenu) : MenuBoard에 들어갈 음료 메뉴 맵을 리턴하는 enum 클래스
    menuBoard가 비어있을 때만 해시맵을 생성한다.

---
+++ src/main/java/christmas/menus/BeverageMenu.java
@@ -0,0 +1,45 @@
+package christmas.menus;
+/*<애피타이저>
+양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)
+
+<메인>
+티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)
+
+<디저트>
+초코케이크(15,000), 아이스크림(5,000)
+
+<음료>
+제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)
+*/
+
+import christmas.MenuBoard;
+import java.util.HashMap;
+import java.util.List;
+
+public enum BeverageMenu {
+    ZERO_COKE("제로콜라", 3_000),
+    RED_WINE("레드와인", 60_000),
+    CHAMPAGNE("샴페인", 25_000);
+
+    private static final HashMap<String, Integer> menuBoard = new HashMap<>();
+    private static final String MENU_TYPE = "BEVERAGE";
+    private final int price;
+    private final String menuName;
+
+    BeverageMenu(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    private static void setMenuBoard() {
+        List<BeverageMenu> menus = List.of(BeverageMenu.values());
+        menus.forEach(menu -> menuBoard.put(menu.menuName, menu.price));
+    }
+
+    public static MenuBoard getMenuBoard() {
+        if (menuBoard.isEmpty()) {
+            setMenuBoard();
+        }
+        return new MenuBoard(menuBoard, MENU_TYPE);
+    }
+}
