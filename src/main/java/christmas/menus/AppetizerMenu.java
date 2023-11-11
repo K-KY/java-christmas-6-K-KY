dc5e444957975ab40e606c23da6daccabe67e6a2
K-KY <kyuyoungk@naver.com>
2023-11-12 00:40:07 +0900

    Feat(AppetizerMenu) : MenuBoard에 들어갈 애피타이저 메뉴 맵을 리턴하는 enum 클래스
    menuBoard가 비어있을 때만 해시맵을 생성한다.

---
+++ src/main/java/christmas/menus/AppetizerMenu.java
@@ -0,0 +1,34 @@
+package christmas.menus;
+
+import christmas.MenuBoard;
+import java.util.HashMap;
+import java.util.List;
+
+public enum AppetizerMenu {
+    MUSHROOM_SOUP("양송이 수프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저 샐러드", 8_000);
+
+
+    private static final String MENU_TYPE = "Appetizer";
+    private static final HashMap<String, Integer> menuBoard = new HashMap<>();
+    private final String menuName;
+    private final int price;
+
+    AppetizerMenu(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    private static void setMenuBoard() {
+        List<AppetizerMenu> menus = List.of(AppetizerMenu.values());
+        menus.forEach(menu -> menuBoard.put(menu.menuName, menu.price));
+    }
+
+    public static MenuBoard getBoard() {
+        if (menuBoard.isEmpty()) {
+            setMenuBoard();
+        }
+        return new MenuBoard(menuBoard, MENU_TYPE);
+    }
+}
