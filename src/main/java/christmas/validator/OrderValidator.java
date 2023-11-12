253bc71ecd9f54a123cefed04ea7194ab616aef8
K-KY <kyuyoungk@naver.com>
2023-11-13 04:19:02 +0900

    Feat(OrderValidator) : 주문 검증
    존재하는 메뉴인지
    유효한 수량을 입력했는지

---
+++ src/main/java/christmas/validator/OrderValidator.java
@@ -0,0 +1,36 @@
+package christmas.validator;
+
+import christmas.MenuCategory;
+import java.util.List;
+
+public class OrderValidator {
+
+    private static final String DIGIT = "[0-9]+";
+
+    public static void isExist(String order, List<MenuCategory> menus) {
+        if (checkMenu(order, menus)) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    private static Boolean checkMenu(String order, List<MenuCategory> menus) {
+        return menus.stream()
+                .map(menu -> menu.isExist(order))
+                .filter(result -> result)
+                .findFirst().orElse(false);
+    }
+
+    public static void isValidAmount(String amount) {
+        if (amount.matches(DIGIT)) {
+            return;
+        }
+        throw new IllegalStateException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    public static void isAmountInRange(int amount) {
+        if (amount >= 21 || amount <= 0) {
+            throw new IllegalStateException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+}
