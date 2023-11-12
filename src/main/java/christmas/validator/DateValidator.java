7c1dbdafdcb51399b939a2fbe33e08a49dfc179b
K-KY <kyuyoungk@naver.com>
2023-11-13 02:52:36 +0900

    Feat(DateValidator) : 입력된 날짜를 검증

---
+++ src/main/java/christmas/validator/DateValidator.java
@@ -0,0 +1,19 @@
+package christmas.validator;
+
+public class DateValidator {
+
+    private static final String DIGIT = "[1-9]+";
+
+    public static void isNumber(String number) {
+        if (number.matches(DIGIT)) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요.");
+    }
+
+    public static void isInRange(int date) {
+        if (date <= 0 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
