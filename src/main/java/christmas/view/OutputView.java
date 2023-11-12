78b6b4c2cc40e5821e7f70be82e6902eb871b9d6
K-KY <kyuyoungk@naver.com>
2023-11-13 02:15:14 +0900

    Feat(OutputView) : 주말, 평일 할인 안내 메세지

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,7 @@
 package christmas.view;
 
 import christmas.ChristmasBadge;
+import christmas.MenuCategory;
 
 public class OutputView {
 
@@ -9,5 +10,15 @@ public class OutputView {
             System.out.println("고객님은 " + badge.getName(badge) +" 배지를 획득 하셨습니다!");
             System.out.println("이후에 진행 될 새해 이벤트에서 배지에 따른 선물이 증정됩니다.");
         }
+    }
+
+    public static void printWeekDayDiscountAnnouncement() {
+        System.out.println("평일 할인이 적용됩니다!");
+        System.out.println("10,000원 이상 주문하면 모든 디저트메뉴 2023원 할인");
+    }
+
+    public static void printWeekendDiscountAnnouncement() {
+        System.out.println("주말 할인이 적용됩니다!");
+        System.out.println("10,000원 이상 주문하면 모든 메인메뉴 2023원 할인");
     }
 }
