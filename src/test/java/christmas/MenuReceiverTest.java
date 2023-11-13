19c7c1583860d9636af2b34fc20d191e7d1d0147
K-KY <kyuyoungk@naver.com>
2023-11-14 01:45:04 +0900

    Test(MenuReceiverTest) : MenuReceiver에서 생성자를 인자로 받도록 변경

--- src/test/java/christmas/MenuReceiverTest.java
+++ src/test/java/christmas/MenuReceiverTest.java
@@ -11,47 +11,47 @@ public class MenuReceiverTest {
     @Test
     void normalCase() {
         List<String> orders = List.of("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1".split(","));
-        MenuReceiver menuReceiver = new MenuReceiver();
-        assertThatCode(() -> menuReceiver.receive(orders))
+        MenuReceiver menuReceiver = new MenuReceiver(orders);
+        assertThatCode(menuReceiver::receive)
                 .doesNotThrowAnyException();
     }
 
     @Test
     void onlyBeverage() {
         List<String> orders = List.of("제로콜라-1".split(","));
-        MenuReceiver menuReceiver = new MenuReceiver();
-        assertThatThrownBy(() -> menuReceiver.receive(orders))
+        MenuReceiver menuReceiver = new MenuReceiver(orders);
+        assertThatThrownBy(menuReceiver::receive)
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     void overOrdered() {
         List<String> orders = List.of("티본스테이크-21".split(","));
-        MenuReceiver menuReceiver = new MenuReceiver();
-        assertThatThrownBy(() -> menuReceiver.receive(orders))
+        MenuReceiver menuReceiver = new MenuReceiver(orders);
+        assertThatThrownBy(menuReceiver::receive)
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     void overOrdered_case2() {
         List<String> orders = List.of("티본스테이크-10,아이스크림-11".split(","));
-        MenuReceiver menuReceiver = new MenuReceiver();
-        assertThatThrownBy(() -> menuReceiver.receive(orders))
+        MenuReceiver menuReceiver = new MenuReceiver(orders);
+        assertThatThrownBy(menuReceiver::receive)
                 .isInstanceOf(IllegalStateException.class);
     }
     @Test
     void amountNotNumber() {
         List<String> orders = List.of("티본스테이크-10,아이스크림-a".split(","));
-        MenuReceiver menuReceiver = new MenuReceiver();
-        assertThatThrownBy(() -> menuReceiver.receive(orders))
+        MenuReceiver menuReceiver = new MenuReceiver(orders);
+        assertThatThrownBy(menuReceiver::receive)
                 .isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     void menuNotExist() {
         List<String> orders = List.of("고등어-10".split(","));
-        MenuReceiver menuReceiver = new MenuReceiver();
-        assertThatThrownBy(() -> menuReceiver.receive(orders))
+        MenuReceiver menuReceiver = new MenuReceiver(orders);
+        assertThatThrownBy(menuReceiver::receive)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
