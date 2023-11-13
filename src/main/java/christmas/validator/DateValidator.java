4f737e66ede7de43671c2649f7e1e7ca118bbf9d
K-KY <kyuyoungk@naver.com>
2023-11-14 02:58:41 +0900

    Fix(DateValidator) : 출력 문구 수정

--- src/main/java/christmas/validator/DateValidator.java
+++ src/main/java/christmas/validator/DateValidator.java
@@ -8,7 +8,7 @@ public class DateValidator {
         if (number.matches(DIGIT)) {
             return;
         }
-        throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요.");
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
     public static void isInRange(int date) {
