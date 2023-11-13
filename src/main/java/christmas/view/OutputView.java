c0c977fa3d401a8e3d03974ccfbd09af1960fb79
K-KY <kyuyoungk@naver.com>
2023-11-14 01:17:50 +0900

    Feat(OutputView) : 메뉴와 메뉴 입력 메세지 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,13 +1,18 @@
 package christmas.view;
 
 import christmas.ChristmasBadge;
-import christmas.MenuCategory;
+import christmas.MenuBoard;
 
 public class OutputView {
 
     public static void startMessage() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
+    public static void menuAndAmount(MenuBoard menuBoard) {
+        System.out.println(menuBoard);
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
     }
 
     public static void badgeAnnouncement(ChristmasBadge badge) {
