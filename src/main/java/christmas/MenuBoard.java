18e20fb7effc9c50687effd0bbe13d44c0b38526
K-KY <kyuyoungk@naver.com>
2023-11-13 18:55:15 +0900

    Feat(MenuBoard) : 모든 메뉴를 담을 컬렉션

---
+++ src/main/java/christmas/MenuBoard.java
@@ -0,0 +1,32 @@
+package christmas;
+
+import christmas.menus.AppetizerMenu;
+import christmas.menus.BeverageMenu;
+import christmas.menus.DessertMenu;
+import christmas.menus.MainMenu;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuBoard {
+    private static final List<MenuCategory> menuCategories = new ArrayList<>();
+    private static final String NEW_LINE = "\n";
+
+    public MenuBoard() {
+        menuCategories.clear();
+        loadMenuCategory();
+    }
+
+    private void loadMenuCategory() {
+        menuCategories.add(AppetizerMenu.getMenuCategory());
+        menuCategories.add(MainMenu.getMenuCategory());
+        menuCategories.add(DessertMenu.getMenuCategory());
+        menuCategories.add(BeverageMenu.getMenuCategory());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        menuCategories.forEach(category -> stringBuilder.append(category.toString()).append(NEW_LINE));
+        return stringBuilder.toString();
+    }
+}
