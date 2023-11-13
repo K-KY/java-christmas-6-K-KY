da9ddc25f24e53bc04e510875f6349328e91f413
K-KY <kyuyoungk@naver.com>
2023-11-13 15:34:31 +0900

    Feat(OutputView) : 출력할 메세지 추가
    시작 메세지
    특별 할인 메세지

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,6 +5,11 @@ import christmas.MenuCategory;
 
 public class OutputView {
 
+    public static void startMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+
     public static void badgeAnnouncement(ChristmasBadge badge) {
         if (badge != ChristmasBadge.NO_BADGE) {
             System.out.println("고객님은 " + badge.getName(badge) +" 배지를 획득 하셨습니다!");
@@ -25,5 +30,10 @@ public class OutputView {
     public static void printChristMasDiscountAnnouncement(int discount) {
         System.out.println("크리스마스 디데이 할인이 적용됩니다!");
         System.out.println("10000원 이상 주문 하면 총금액에서 " + discount +"원 할인");
+    }
+
+    public static void printSpecialDiscountAnnouncement() {
+        System.out.println("특별 할인 이벤트가 적용됩니다!");
+        System.out.println("총 주문 금액에서 1000원 할인");
     }
 }
