1d35f00d1041ada99fef7712e2175315d6a144fc
K-KY <kyuyoungk@naver.com>
2023-11-13 21:23:49 +0900

    Fix(MainMenu) : 오타 수정

--- src/main/java/christmas/menus/MainMenu.java
+++ src/main/java/christmas/menus/MainMenu.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public enum MainMenu {
     T_BONE_STAKE("티본스테이크", 55_000),
-    BARBEQUE_RIP("바베큐립", 54_000),
+    BARBEQUE_RIP("바비큐립", 54_000),
     SEA_FOOD_PASTA("해산물파스타", 35_000),
     CHRISTMAS_PASTA("크리스마스파스타", 25_000);
 
