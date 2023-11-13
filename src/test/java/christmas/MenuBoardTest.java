98eb9df98d884648a1b7a3689a871a4839213b2a
K-KY <kyuyoungk@naver.com>
2023-11-13 18:55:46 +0900

    Test(MenuBoardTest) : toString이 메뉴를 리턴하는지 테스트

---
+++ src/test/java/christmas/MenuBoardTest.java
@@ -0,0 +1,19 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class MenuBoardTest {
+
+    @Test
+    void menuBoardToString() {
+        MenuBoard menuBoard = new MenuBoard();
+        assertThat(menuBoard.toString())
+                .contains("<애피타이저>")
+                .contains("<메인>")
+                .contains("<디저트>")
+                .contains("<음료>");
+
+    }
+}
