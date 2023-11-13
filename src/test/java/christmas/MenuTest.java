9ad9390efe21bfc283683de9fbfdc568d5fa711d
K-KY <kyuyoungk@naver.com>
2023-11-13 20:40:04 +0900

    Test : 검증 방식 변경으로 테스트 수정
    MenuTestTest
    OrderValidateTest
    UserOrderTest
    의 메뉴 카테고리를 인자로 받는부분 제거

--- src/test/java/christmas/MenuTest.java
+++ src/test/java/christmas/MenuTest.java
@@ -3,41 +3,25 @@ package christmas;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import christmas.menus.AppetizerMenu;
-import christmas.menus.BeverageMenu;
-import christmas.menus.DessertMenu;
-import christmas.menus.MainMenu;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MenuTest {
-    private List<MenuCategory> menuCategories = new ArrayList<>();
-
-    @BeforeEach
-    void setUp() {
-        menuCategories.add(AppetizerMenu.getMenuCategory());
-        menuCategories.add(BeverageMenu.getMenuCategory());
-        menuCategories.add(DessertMenu.getMenuCategory());
-        menuCategories.add(MainMenu.getMenuCategory());
-    }
 
     @Test
     void notExistMenu() {
-        assertThatThrownBy(() -> new Menu("고등어", 1, menuCategories))
+        assertThatThrownBy(() -> new Menu("고등어", 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void existMenu() {
-        assertThatCode(() -> new Menu("아이스크림", 1, menuCategories))
+        assertThatCode(() -> new Menu("아이스크림", 1))
                 .doesNotThrowAnyException();
     }
 
     @Test
     void bigAmount() {
-        assertThatThrownBy(() -> new Menu("아이스크림", 99, menuCategories))
+        assertThatThrownBy(() -> new Menu("아이스크림", 99))
                 .isInstanceOf(IllegalStateException.class);
     }
 }
--- src/test/java/christmas/UserOrderTest.java
+++ src/test/java/christmas/UserOrderTest.java
@@ -3,43 +3,28 @@ package christmas;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import christmas.menus.AppetizerMenu;
-import christmas.menus.BeverageMenu;
-import christmas.menus.DessertMenu;
-import christmas.menus.MainMenu;
-import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserOrderTest {
-    private List<MenuCategory> menuCategories = new ArrayList<>();
-
-    @BeforeEach
-    void setUp() {
-        menuCategories.add(AppetizerMenu.getMenuCategory());
-        menuCategories.add(BeverageMenu.getMenuCategory());
-        menuCategories.add(DessertMenu.getMenuCategory());
-        menuCategories.add(MainMenu.getMenuCategory());
-    }
 
     @Test
     void userOrderAmount() {
-        Menu menu = new Menu("아이스크림", 10, menuCategories);
+        Menu menu = new Menu("아이스크림", 10);
         assertThatCode(() -> new UserOrder(List.of(menu)))
                 .doesNotThrowAnyException();
     }
 
     @Test
     void userOrderBigAmount() {
-        Menu menu = new Menu("아이스크림", 19, menuCategories);
-        Menu menu2 = new Menu("초코케이크", 19, menuCategories);
+        Menu menu = new Menu("아이스크림", 19);
+        Menu menu2 = new Menu("초코케이크", 19);
         assertThatThrownBy(() -> new UserOrder(List.of(menu, menu2)))
                 .isInstanceOf(IllegalStateException.class);
     }
     @Test
     void userOrderOnlyBeverage() {
-        Menu menu = new Menu("제로콜라", 19, menuCategories);
+        Menu menu = new Menu("제로콜라", 19);
         assertThatThrownBy(() -> new UserOrder(List.of(menu)))
                 .isInstanceOf(IllegalStateException.class);
     }
