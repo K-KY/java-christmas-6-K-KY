f4bae8c1432f48ad11750facee3ea6ff5eac0c20
K-KY <kyuyoungk@naver.com>
2023-11-14 03:03:01 +0900

    Feat(PlannerController) : 올바른 값이 입력 될 때까지 메뉴를 입력받는 메소드

--- src/main/java/christmas/PlannerController.java
+++ src/main/java/christmas/PlannerController.java
@@ -1,12 +1,27 @@
 package christmas;
 
 import christmas.view.InputView;
+import java.util.List;
 
 public class PlannerController {
+    public static final MenuBoard menuBoard = new MenuBoard();
 
     public void start() {
         Date date = callDate();
+        UserOrder userOrder = callUserOrder();
+    }
 
+    private UserOrder callUserOrder() {
+        UserOrder userOrder;
+        try {
+            String userMenu = InputView.orderMenu(menuBoard);
+            MenuReceiver menuReceiver = new MenuReceiver(List.of(userMenu.split(",")));
+            userOrder = menuReceiver.receive();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            userOrder = callUserOrder();
+        }
+        return userOrder;
     }
 
     private Date callDate() {
@@ -15,7 +30,7 @@ public class PlannerController {
             int input = InputView.visitDate();
             date = new Date(input);
         } catch (IllegalArgumentException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
             date = callDate();
         }
         return date;
