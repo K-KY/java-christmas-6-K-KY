405e23d3db7466d51982d2892eb4c86841841307
K-KY <kyuyoungk@naver.com>
2023-11-15 08:18:52 +0900

    Refactor(Benefit) : 문자열 매직넘버 상수로 선언

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Benefit {
+    private static final String NEW_LINE = "\n";
     private final List<String> ownBenefit = new ArrayList<>();
 
     public void addBenefit(String benefit) {
@@ -13,7 +14,7 @@ public class Benefit {
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        ownBenefit.forEach(benefits -> stringBuilder.append(benefits).append("\n"));
+        ownBenefit.forEach(benefits -> stringBuilder.append(benefits).append(NEW_LINE));
         return stringBuilder.toString();
     }
 }
