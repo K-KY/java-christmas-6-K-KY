440e0f8d2dbbeff8e18f19027f1a4401c9d03702
K-KY <kyuyoungk@naver.com>
2023-11-14 21:30:33 +0900

    Refactor(WeekDayDiscounter) : 할인 적용 후 할인 금액을 리턴하도록 변경

--- src/main/java/christmas/discounter/WeekDayDiscounter.java
+++ src/main/java/christmas/discounter/WeekDayDiscounter.java
@@ -2,6 +2,7 @@ package christmas.discounter;
 
 import christmas.Menu;
 import christmas.MenuCategory;
+import christmas.OrderedAmount;
 import christmas.UserOrder;
 import christmas.menus.DessertMenu;
 import java.util.ArrayList;
@@ -11,11 +12,12 @@ import java.util.stream.Collectors;
 public class WeekDayDiscounter {
     private static final MenuCategory dessertCategory = DessertMenu.getMenuCategory();
     private static final List<Integer> amount = new ArrayList<>();
+    private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
 
     public long discount(UserOrder userOrder) {
         List<Menu> orders = userOrder.orders;
         long dessertCount = dessertCount(orders);
-        System.out.println(dessertCount);
+        orderedAmount.discount(dessertCount * 2023);
         return 2023 * dessertCount;
     }
 
