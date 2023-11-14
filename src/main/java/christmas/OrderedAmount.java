727782e2918636b8d677073088821500774e2622
K-KY <kyuyoungk@naver.com>
2023-11-14 19:27:37 +0900

    Feat(OrderedAmount) : 만원 이상인지 확인하는 메서드, 할인 금액을 모두 더하는 메서드
    기본생성자 추가

--- src/main/java/christmas/OrderedAmount.java
+++ src/main/java/christmas/OrderedAmount.java
@@ -4,6 +4,7 @@ public class OrderedAmount {
 
     private static OrderedAmount orderedAmount;
     private static int totalAmount = 0;
+    private static int totalDiscount = 0;
     private OrderedAmount(){}
 
     public static OrderedAmount getOrderedAmount(int amount) {
@@ -11,8 +12,20 @@ public class OrderedAmount {
             orderedAmount = new OrderedAmount();
         }
         totalAmount+= amount;
-        System.out.println(totalAmount);
+        return orderedAmount;
+    }
+    public static OrderedAmount getOrderedAmount() {
+        if (orderedAmount == null) {
+            orderedAmount = new OrderedAmount();
+        }
         return orderedAmount;
     }
 
+    public boolean isOverTenThousand() {
+        return totalAmount >= 10000;
+    }
+
+    public void discount(long discount) {
+        totalDiscount -= discount;
+    }
 }
