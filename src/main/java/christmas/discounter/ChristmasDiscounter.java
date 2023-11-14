4180c95d4ca948c2d35bbaa50c5b8f79324c6a45
K-KY <kyuyoungk@naver.com>
2023-11-15 02:38:42 +0900

    Refactor(ChristmasDiscounter) : 리턴하는 대신 기록을 저장

--- src/main/java/christmas/discounter/ChristmasDiscounter.java
+++ src/main/java/christmas/discounter/ChristmasDiscounter.java
@@ -1,5 +1,6 @@
 package christmas.discounter;
 
+import christmas.Benefit;
 import christmas.Date;
 import christmas.OrderedAmount;
 import java.text.DecimalFormat;
@@ -7,14 +8,13 @@ import java.text.DecimalFormat;
 public class ChristmasDiscounter {
     private static final String PRICE_FORMAT = "-###,###원";
     private static final DecimalFormat MONEY_FORMAT = new DecimalFormat(PRICE_FORMAT);
-
+    private static final Benefit benefit = Benefit.getBenefit();
     private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
 
-    public String discount(Date date) {
+    public void discount(Date date) {
         if (date.isBeforeChristmas() && orderedAmount.isOverTenThousand()) {
             orderedAmount.discount(date.discount());
-            return "크리스마스 디데이 할인: "+ MONEY_FORMAT.format(date.discount());
+            benefit.addBenefit("크리스마스 디데이 할인: "+ MONEY_FORMAT.format(date.discount()));
         }
-        return "";
     }
 }
