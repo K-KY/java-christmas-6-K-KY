e8ce521147762d6b0dbff016f4f47e112a5cb772
K-KY <kyuyoungk@naver.com>
2023-11-15 03:29:14 +0900

    Test(WeekendDiscounterTest) : 주말 할인 테스트

--- src/test/java/christmas/discounter/WeekendDiscounterTest.java
+++ src/test/java/christmas/discounter/WeekendDiscounterTest.java
@@ -2,14 +2,20 @@ package christmas.discounter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.domain.MenuReceiver;
+import christmas.domain.UserOrder;
+import christmas.domain.discounter.WeekendDiscounter;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class WeekendDiscounterTest {
 
     @Test
-    void discountTest() {
-        WeekendDiscounter discounter = new WeekendDiscounter();
-        assertThat(discounter.discount(2, 109000)).isEqualTo(104954);
-    }
+    void orderTest() {
+        MenuReceiver menuReceiver = new MenuReceiver(List.of("티본스테이크-1,아이스크림-1,타파스-1".split(",")));
+        UserOrder userOrder = menuReceiver.receive();
 
+        WeekendDiscounter weekendDiscounter = new WeekendDiscounter();
+        weekendDiscounter.discount(userOrder);
+    }
 }
