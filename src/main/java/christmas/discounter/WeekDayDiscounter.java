ab88543588753128e8a13b5ac0b36ad8cb35e785
K-KY <kyuyoungk@naver.com>
2023-11-14 19:10:54 +0900

    Feat(WeekDayDiscounter) : 디저트메뉴의 수에따라 할인 금액을 리턴

--- src/main/java/christmas/discounter/WeekDayDiscounter.java
+++ src/main/java/christmas/discounter/WeekDayDiscounter.java
@@ -1,11 +1,30 @@
 package christmas.discounter;
 
-public class WeekDayDiscounter {
+import christmas.Menu;
+import christmas.MenuCategory;
+import christmas.UserOrder;
+import christmas.menus.DessertMenu;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public int discount(int dessertAmount, int orderAmount) {
-        if (orderAmount >= 10000) {
-            orderAmount -= dessertAmount * 2023;
-        }
-        return orderAmount;
+public class WeekDayDiscounter {
+    private static final MenuCategory dessertCategory = DessertMenu.getMenuCategory();
+    private static final List<Integer> amount = new ArrayList<>();
+
+    public long discount(UserOrder userOrder) {
+        List<Menu> orders = userOrder.orders;
+        long dessertCount = dessertCount(orders);
+        System.out.println(dessertCount);
+        return 2023 * dessertCount;
     }
+
+    private static long dessertCount(List<Menu> orders) {
+        orders.stream()
+                .filter(order -> order.isDessert(dessertCategory))
+                .map(order -> order.addAmount(amount))
+                .collect(Collectors.toList());
+        return amount.stream().mapToInt(Integer::intValue).sum();
+    }
+
 }
