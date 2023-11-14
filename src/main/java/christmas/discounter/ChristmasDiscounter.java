aa706919f7f9710b4acc233be4cdee039bfe85d7
K-KY <kyuyoungk@naver.com>
2023-11-14 21:59:57 +0900

    Refactor(ChristmasDiscounter) : 할인 내역을 리턴하도록 변경

--- src/main/java/christmas/discounter/ChristmasDiscounter.java
+++ src/main/java/christmas/discounter/ChristmasDiscounter.java
@@ -2,15 +2,19 @@ package christmas.discounter;
 
 import christmas.Date;
 import christmas.OrderedAmount;
+import java.text.DecimalFormat;
 
 public class ChristmasDiscounter {
+    private static final String PRICE_FORMAT = "-###,###원";
+    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat(PRICE_FORMAT);
+
     private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
 
-    public int discount(Date date) {
+    public String discount(Date date) {
         if (date.isBeforeChristmas() && orderedAmount.isOverTenThousand()) {
             orderedAmount.discount(date.discount());
-            return date.discount();
+            return "크리스마스 디데이 할인: "+ MONEY_FORMAT.format(date.discount());
         }
-        return 0;
+        return "";
     }
 }
