3d52c2cb6220c8808f4203a479ef04dc61f32b99
K-KY <kyuyoungk@naver.com>
2023-11-15 07:13:00 +0900

    Refactor(WeekendDiscounterTest) : 테스트 수정

--- src/test/java/christmas/discounter/WeekendDiscounterTest.java
+++ src/test/java/christmas/discounter/WeekendDiscounterTest.java
@@ -2,6 +2,7 @@ package christmas.discounter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.domain.Benefit;
 import christmas.domain.MenuReceiver;
 import christmas.domain.UserOrder;
 import christmas.domain.discounter.WeekendDiscounter;
@@ -12,10 +13,13 @@ public class WeekendDiscounterTest {
 
     @Test
     void orderTest() {
+        Benefit benefit = new Benefit();
         MenuReceiver menuReceiver = new MenuReceiver(List.of("티본스테이크-1,아이스크림-1,타파스-1".split(",")));
         UserOrder userOrder = menuReceiver.receive();
 
         WeekendDiscounter weekendDiscounter = new WeekendDiscounter();
-        weekendDiscounter.discount(userOrder);
+        weekendDiscounter.discount(userOrder, benefit);
+
+        assertThat(benefit.toString()).isEqualTo("주말 할인: -2,023원\n");
     }
 }
