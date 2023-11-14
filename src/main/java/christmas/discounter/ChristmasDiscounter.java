88ee016f5cc108701fee4c134407a040b8721af1
K-KY <kyuyoungk@naver.com>
2023-11-14 21:29:37 +0900

    Feat(ChristmasDiscounter) : 주문 금액이 만원 이상이어야 할인 되도록 변경

--- src/main/java/christmas/discounter/ChristmasDiscounter.java
+++ src/main/java/christmas/discounter/ChristmasDiscounter.java
@@ -4,10 +4,11 @@ import christmas.Date;
 import christmas.OrderedAmount;
 
 public class ChristmasDiscounter {
-
+    private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
 
     public int discount(Date date) {
-        if (date.isBeforeChristmas()) {
+        if (date.isBeforeChristmas() && orderedAmount.isOverTenThousand()) {
+            orderedAmount.discount(date.discount());
             return date.discount();
         }
         return 0;
