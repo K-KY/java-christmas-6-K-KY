bb72f4023bc87bbe9aa14f4dd75235d200ba8b81
K-KY <kyuyoungk@naver.com>
2023-11-14 19:10:34 +0900

    Feat(WeekendDiscounter) : 메인메뉴의 수에따라 할인 금액을 리턴

--- src/main/java/christmas/discounter/WeekendDiscounter.java
+++ src/main/java/christmas/discounter/WeekendDiscounter.java
@@ -1,11 +1,29 @@
 package christmas.discounter;
 
+import christmas.Menu;
+import christmas.MenuCategory;
+import christmas.UserOrder;
+import christmas.menus.MainMenu;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WeekendDiscounter {
 
-    public int discount(int mainAmount, int orderAmount) {
-        if (orderAmount >= 10000) {
-            orderAmount -= mainAmount * 2023;
-        }
-        return orderAmount;
+    public static final MenuCategory mainCategory = MainMenu.getMenuCategory();
+    private static final List<Integer> amount = new ArrayList<>();
+
+    public long discount(UserOrder userOrder) {
+        List<Menu> orders = userOrder.orders;
+        long mainCount = mainCount(orders);
+        return 2023 * mainCount;
+    }
+
+    private static long mainCount(List<Menu> orders) {
+        orders.stream()
+                .filter(order -> order.isMain(mainCategory))
+                .map(order -> order.addAmount(amount))
+                .collect(Collectors.toList());
+        return amount.stream().mapToInt(Integer::intValue).sum();
     }
 }
