a9d4662e7f83ad004c4acd5d4a254de3d56fb78e
K-KY <kyuyoungk@naver.com>
2023-11-14 21:59:38 +0900

    Refactor(WeekendDiscounter) : 할인 내역을 리턴하도록 변경

--- src/main/java/christmas/discounter/WeekendDiscounter.java
+++ src/main/java/christmas/discounter/WeekendDiscounter.java
@@ -5,21 +5,26 @@ import christmas.MenuCategory;
 import christmas.OrderedAmount;
 import christmas.UserOrder;
 import christmas.menus.MainMenu;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class WeekendDiscounter {
-
+    private static final String PRICE_FORMAT = "-###,###원";
+    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat(PRICE_FORMAT);
     public static final MenuCategory mainCategory = MainMenu.getMenuCategory();
     private static final List<Integer> amount = new ArrayList<>();
     private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
 
-    public long discount(UserOrder userOrder) {
+    public String discount(UserOrder userOrder) {
         List<Menu> orders = userOrder.orders;
         long mainCount = mainCount(orders);
+        if (mainCount == 0) {
+            return "";
+        }
         orderedAmount.discount(mainCount * 2023);
-        return 2023 * mainCount;
+        return "주말 할인: " + MONEY_FORMAT.format(2023 * mainCount);
     }
 
     private static long mainCount(List<Menu> orders) {
