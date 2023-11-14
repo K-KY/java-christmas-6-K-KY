f226b371e5dc378b3b712b26947bdf346a6f273c
K-KY <kyuyoungk@naver.com>
2023-11-15 03:26:10 +0900

    Refactor(Application) : 파일위치 변경으로 인한 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,5 +1,7 @@
 package christmas;
 
+import christmas.controller.PlannerController;
+
 public class Application {
     public static void main(String[] args) {
         PlannerController plannerController = new PlannerController();
