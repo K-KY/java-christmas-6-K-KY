7a7a9446710243c05e894a246920315a834d907d
K-KY <kyuyoungk@naver.com>
2023-11-15 02:39:17 +0900

    Refactor(SpecialDiscounter) : 리턴하는 대신 기록을 저장

--- src/main/java/christmas/discounter/SpecialDiscounter.java
+++ src/main/java/christmas/discounter/SpecialDiscounter.java
@@ -1,5 +1,6 @@
 package christmas.discounter;
 
+import christmas.Benefit;
 import christmas.Date;
 import christmas.OrderedAmount;
 import java.text.DecimalFormat;
@@ -8,18 +9,19 @@ public class SpecialDiscounter {
     private static final String PRICE_FORMAT = "-###,###원";
     private static final DecimalFormat MONEY_FORMAT = new DecimalFormat(PRICE_FORMAT);
     private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
+    private static final Benefit benefit = Benefit.getBenefit();
     private static boolean conditionally = false; // true 일 때 총 금액이 만원 이상
-    public String discount(Date date) {
+    public void discount(Date date) {
         String day = date.orderedDate();
         if (sunDayOrDay(date, day) && !conditionally) {
             orderedAmount.discount(1000);
-            return "특별 할인: " + MONEY_FORMAT.format(1000);
+            benefit.addBenefit("특별 할인: " + MONEY_FORMAT.format(1000));
+            return;
         }
         if (sunDayOrDay(date, day) && orderedAmount.isOverTenThousand()) {
             orderedAmount.discount(1000);
-            return "특별 할인: " + MONEY_FORMAT.format(1000);
+            benefit.addBenefit("특별 할인: " + MONEY_FORMAT.format(1000));
         }
-        return "";
     }
 
     private static boolean sunDayOrDay(Date date, String day) {
