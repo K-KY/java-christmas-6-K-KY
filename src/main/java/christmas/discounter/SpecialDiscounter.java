14f29c2f1fda13981df088758598ef8f01d51d42
K-KY <kyuyoungk@naver.com>
2023-11-14 21:59:50 +0900

    Refactor(SpecialDiscounter) : 할인 내역을 리턴하도록 변경

--- src/main/java/christmas/discounter/SpecialDiscounter.java
+++ src/main/java/christmas/discounter/SpecialDiscounter.java
@@ -2,21 +2,24 @@ package christmas.discounter;
 
 import christmas.Date;
 import christmas.OrderedAmount;
+import java.text.DecimalFormat;
 
 public class SpecialDiscounter {
+    private static final String PRICE_FORMAT = "-###,###원";
+    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat(PRICE_FORMAT);
     private static final OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
     private static boolean conditionally = false; // true 일 때 총 금액이 만원 이상
-    public int discount(Date date) {
+    public String discount(Date date) {
         String day = date.orderedDate();
         if (sunDayOrDay(date, day) && !conditionally) {
             orderedAmount.discount(1000);
-            return 1000;
+            return "특별 할인: " + MONEY_FORMAT.format(1000);
         }
         if (sunDayOrDay(date, day) && orderedAmount.isOverTenThousand()) {
             orderedAmount.discount(1000);
-            return 1000;
+            return "특별 할인: " + MONEY_FORMAT.format(1000);
         }
-        return 0;
+        return "";
     }
 
     private static boolean sunDayOrDay(Date date, String day) {
