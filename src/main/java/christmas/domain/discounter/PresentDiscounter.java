a9e1adea46a40527bba0a49b3737f2967facc8f1
K-KY <kyuyoungk@naver.com>
2023-11-15 07:05:20 +0900

    Refactor(PresentDiscounter) : OrderedAmount를 생성자로 주입받도록 변경

--- src/main/java/christmas/domain/discounter/PresentDiscounter.java
+++ src/main/java/christmas/domain/discounter/PresentDiscounter.java
@@ -3,11 +3,15 @@ package christmas.domain.discounter;
 import christmas.domain.OrderedAmount;
 
 public class PresentDiscounter {
-    OrderedAmount orderedAmount = OrderedAmount.getOrderedAmount();
+    private final OrderedAmount orderedAmount;
+
+    public PresentDiscounter(OrderedAmount orderedAmount) {
+        this.orderedAmount = orderedAmount;
+    }
 
     public String discount() {
         if (orderedAmount.present()) {
-            return "샴패인 1개";
+            return "샴페인 1개";
         }
         return "";
     }
