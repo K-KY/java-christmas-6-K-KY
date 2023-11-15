97aeaedc594c784fd7a8505eff4b86475b703a1d
K-KY <kyuyoungk@naver.com>
2023-11-15 09:53:05 +0900

    Test(WeekDayDiscounterTest) : 디저트 메뉴 주문하지 않는경우 추가

--- src/test/java/christmas/discounter/WeekDayDiscounterTest.java
+++ src/test/java/christmas/discounter/WeekDayDiscounterTest.java
@@ -1,5 +1,6 @@
 package christmas.discounter;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import christmas.domain.Benefit;
@@ -7,22 +8,36 @@ import christmas.domain.MenuReceiver;
 import christmas.domain.UserOrder;
 import christmas.domain.discounter.WeekDayDiscounter;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class WeekDayDiscounterTest {
     Benefit benefit = new Benefit();
 
-
     @Test
-    void orderTest() {
+    @DisplayName("평일엔 디저트메뉴 2023원 할인")
+    void hasDessert() {
 
         MenuReceiver menuReceiver = new MenuReceiver(List.of("티본스테이크-1,아이스크림-1,타파스-1".split(",")));
         UserOrder userOrder = menuReceiver.receive();
 
         WeekDayDiscounter weekDayDiscounter = new WeekDayDiscounter();
         benefit = new Benefit();
-        weekDayDiscounter.discount(userOrder, benefit);
-
+        assertThat(weekDayDiscounter.discount(userOrder, benefit)).isEqualTo(2023);
         assertThat(benefit.toString()).isEqualTo("평일 할인: -2,023원\n");
+    }
+
+    @Test
+    @DisplayName("디저트메뉴가 없으면 평일 할인 0원")
+    void noDiscount() {
+
+        MenuReceiver menuReceiver = new MenuReceiver(List.of("티본스테이크-1,타파스-1".split(",")));
+        UserOrder userOrder = menuReceiver.receive();
+
+        WeekDayDiscounter weekDayDiscounter = new WeekDayDiscounter();
+        benefit = new Benefit();
+
+        assertThat(weekDayDiscounter.discount(userOrder, benefit)).isEqualTo(0);
+        assertThat(benefit.toString()).isEqualTo("");
     }
 }
